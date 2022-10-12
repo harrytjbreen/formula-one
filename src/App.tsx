ef7ac3f3c386fe518524dcee9d05bd20fbc26426
harrytjbreen <harrytjbreen@gmail.com>
2022-10-12 17:14:26 +0100

    feat: added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import baseTheme from "./themes";
 import {ThemeProvider} from "styled-components";
 
 const App: React.FC = () => {
-  return <ThemeProvider theme={baseTheme}>
-
-  </ThemeProvider>;
+  return <ThemeProvider theme={baseTheme}></ThemeProvider>;
 };
 
 export default App;
--- src/themes.ts
+++ src/themes.ts
@@ -1,16 +1,16 @@
 const baseTheme = {
-    colors: {
-        redbull: "#FF0C49",
-        mercedes: "#00A19C",
-        mclaren: "#E0610E",
-        alphaTauri: "#00293F",
-        alphaRomeo: "#981E32",
-        haas: "#f9f2f2",
-        williams: "#041e42",
-        ferrari: "#a6051a",
-        alpine: "#0090FF",
-        astonMartin: "#006F62",
-    },
+  colors: {
+    redbull: "#FF0C49",
+    mercedes: "#00A19C",
+    mclaren: "#E0610E",
+    alphaTauri: "#00293F",
+    alphaRomeo: "#981E32",
+    haas: "#f9f2f2",
+    williams: "#041e42",
+    ferrari: "#a6051a",
+    alpine: "#0090FF",
+    astonMartin: "#006F62",
+  },
 };
 
 export default baseTheme;
